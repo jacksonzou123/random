ccb5e93a44e3f619b50969a533ad04abf78e26d9
Jackson <jzou00@stuy.edu>
2019-11-11 16:51:39 -0500

    bleh

--- main.c
+++ main.c
@@ -8,11 +8,12 @@
 
 int main() {
   int *list = malloc(10 * sizeof(int));
-  int num = generateRandom();
-  printf("num: %d\n", num);
   int i;
   for (i = 0; i < 10; i++) {
-    printf("printing num %d: %d\n", i, *(list+i));
+    *(list+i) = generateRandom();
+  }
+  for (i = 0; i < 10; i++) {
+    printf("printing num %d: %d", i, *(list+i));
   }
   return 0;
 }
